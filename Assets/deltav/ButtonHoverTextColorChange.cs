3849e9e76b7249e54a64721a0a485ab7d147330f
ACReeser <rogueconformist@gmail.com>
2018-12-25 17:35:45 -0600

    Mock UI for planet and city

---
+++ Assets/deltav/ButtonHoverTextColorChange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverTextColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text buttonText;
+    private Color nonHoverColor;
+    public Color HoverColor;
+
+    void Start()
+    {
+        nonHoverColor = buttonText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.color = HoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.color = nonHoverColor;
+    }
+}
+
