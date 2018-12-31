903751c544122d37e9c0ee309a8579e2a15d01ae
ACReeser <rogueconformist@gmail.com>
2018-12-31 00:11:27 -0600

    Almost done with new infrastructure UI

--- Assets/deltav/UI/CanvasManager.cs
+++ Assets/deltav/UI/CanvasManager.cs
@@ -1,19 +1,106 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
+
+[Serializable]
+public class ConstructionView
+{
+    public RectTransform ParentFrame;
+    public RectTransform InfrastructureButtonPrefab;
+    public RectTransform EmptyButtonPrefab;
+
+    private RectTransform[] InfrastructureButtons = new RectTransform[12];
+    private RectTransform[] EmptyButtons = new RectTransform[12];
+
+    public CanvasManager CanvasMan;
+
+    public void Fill(City c)
+    {
+        var infras = c.Infrastructure.Values.ToArray();
+        for (int i = 0; i < 12; i++)
+        {
+            Infrastructure structure = null;
+            if (i < infras.Length)
+            {
+                structure = infras[i];
+                if (InfrastructureButtons[i] == null)
+                {
+                    InfrastructureButtons[i] = GameObject.Instantiate(InfrastructureButtonPrefab, ParentFrame);
+                }
+                else
+                {
+                    InfrastructureButtons[i].gameObject.SetActive(true);
+                }
+                InfrastructureButtons[i].GetChild(0).GetComponent<Text>().text = CanvasManager.ToSentenceCase(structure.Type.ToString());
+
+                if (EmptyButtons[i] != null)
+                {
+                    EmptyButtons[i].gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                if (EmptyButtons[i] == null)
+                {
+                    EmptyButtons[i] = GameObject.Instantiate(EmptyButtonPrefab, ParentFrame);
+                    EmptyButtons[i].GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        CanvasMan.ToggleNewBuild(true);
+                    });
+                }
+                else
+                {
+                    EmptyButtons[i].gameObject.SetActive(true);
+                }
+
+                if (InfrastructureButtons[i] != null)
+                {
+                    InfrastructureButtons[i].gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
+
+[Serializable]
+public class RegisterView
+{
+    public Text Name, Population;
+    public Text Growth, Assembly, Metal, Energy;
+
+    public void Fill(Planet p)
+    {
+        Name.text = " > " + p.Name.ToUpperInvariant();
+        Assembly.text = p.Empires[PhaseManager.S.PlayerID].CurrentResources.Assembly + " Assembly";
+        Metal.text = p.Empires[PhaseManager.S.PlayerID].CurrentResources.Metal + " Metal";
+        Energy.text = p.Empires[PhaseManager.S.PlayerID].CurrentResources.Energy + " Energy";
+    }
+    public void Fill(City c)
+    {
+        Name.text = " > " + c.Name.ToUpperInvariant();
+        var sum = c.GetResourcesPerTurnSummary();
+        Assembly.text = "+"+sum.Assembly + " Assembly";
+        Metal.text = "+" + sum.Metal + " Metal";
+        Energy.text = sum.Energy + " Energy";
+    }
+}
 
 public class CanvasManager : MonoBehaviour {
     public Text PhaseText;
     public RectTransform EndTurnButton;
 
     public RectTransform PlanetPanel;
-    public Text PlanetName;
+    public RegisterView PlanetRegister;
 
 
     public RectTransform CityPanel;
-    public Text CityName;
+    public RegisterView CityRegister;
+    public ConstructionView CityConstruction;
+    public RectTransform NewCityConstructionPanel;
 
     // Use this for initialization
     void Start () {
@@ -21,6 +108,8 @@ public class CanvasManager : MonoBehaviour {
         S_OnPhaseChange(PhaseManager.S.CurrentPhase);
         UnFocusCity();
         UnFocusPlanet();
+        CityConstruction.CanvasMan = this;
+        ToggleNewBuild(false);
     }
 
     private void S_OnPhaseChange(Phase NewPhase)
@@ -37,7 +126,7 @@ public class CanvasManager : MonoBehaviour {
     internal void FocusPlanet(Planet planet)
     {
         PlanetPanel.gameObject.SetActive(true);
-        PlanetName.text = " > " +planet.Name.ToUpperInvariant();
+        PlanetRegister.Fill(planet);
     }
 
     internal void UnFocusPlanet()
@@ -49,7 +138,8 @@ public class CanvasManager : MonoBehaviour {
     internal void FocusCity(City c)
     {
         CityPanel.gameObject.SetActive(true);
-        CityName.text = " > "+c.Name.ToUpperInvariant();
+        CityRegister.Fill(c);
+        CityConstruction.Fill(c);
     }
 
     internal void UnFocusCity()
@@ -60,5 +150,15 @@ public class CanvasManager : MonoBehaviour {
     public void EndTurnButtonClick()
     {
         PhaseManager.S.EndTurn();
+    }
+
+    public static string ToSentenceCase(string str)
+    {
+        return Regex.Replace(str, "[a-z][A-Z]", m => m.Value[0] + " " + m.Value[1]);
+    }
+
+    public void ToggleNewBuild(bool state)
+    {
+        NewCityConstructionPanel.gameObject.SetActive(state);
     }
 }
